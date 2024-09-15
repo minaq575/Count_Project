badf08ea1b95742375d5732feaa883c3e9d57dfd
Napassron Kemkrathok <mintmintgamer@gmail.com>
2024-09-15 18:42:06 +0700

    15/9

--- app/(contents)/count/page.js
+++ app/(contents)/count/page.js
@@ -16,7 +16,7 @@ async function updateCounter(current) {
     body: JSON.stringify({ current })
   };
 
-  const res = await fetch('https://count-project-eta.vercel.app/api/counter', postData);
+  const res = await fetch('http://localhost:3000/api/counter', postData);
   if (!res.ok) {
     throw new Error("Failed to update counter");
   }
